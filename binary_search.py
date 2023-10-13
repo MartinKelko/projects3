68a6b7a106cbc614b1689f68b14881458a7756c6
MartinKelko <martin2kelko@gmail.com>
2023-10-13 12:16:53 +0200

    Commit message

--- binary_search.py
+++ binary_search.py
@@ -2,10 +2,10 @@ import random
 import time
 
 def naive_search(l, target):
-    for i in range(len,(l)):
+    for i in range(len(l)):
         if l[i] == target:
             return i
-        return -1
+    return -1
 
 def binary_search(l, target, low=None, high=None):
     if low is None:
@@ -14,34 +14,29 @@ def binary_search(l, target, low=None, high=None):
         high = len(l) - 1
 
     if high < low:
-        return  -1
+        return -1
 
-    midpoint = (low + high // 2)
+    midpoint = (low + high) // 2
 
     if l[midpoint] == target:
         return midpoint
     elif target < l[midpoint]:
-        return binary_search(l,target, low, midpoint-1)
+        return binary_search(l, target, low, midpoint - 1)
     else:
-        return binary_search(l,target,midpoint+1, high)
+        return binary_search(l, target, midpoint + 1, high)
 
-if __name__=='_main_':
-    #l = [1,3,5,10,12]
-    #target = 10
-    #print(naive_search(l,target))
-    #print(binary_search(l,target))
-
+if __name__ == '__main__':
     length = 10000
     sorted_list = set()
     while len(sorted_list) < length:
-        sorted_list.add(random.randint(-3*length, 3*length))
+        sorted_list.add(random.randint(-3 * length, 3 * length))
     sorted_list = sorted(list(sorted_list))
 
     start = time.time()
     for target in sorted_list:
         naive_search(sorted_list, target)
     end = time.time()
-    print("Naive search time: ", (end-start)/length, "seconds")
+    print("Naive search time: ", (end - start) / length, "seconds")
 
     start = time.time()
     for target in sorted_list:
