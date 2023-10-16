d7ba2944624392c8802526e218825dce66ef8ec1
martin.kelko <martin2kelko@gmail.com>
2023-10-16 10:23:57 +0200

    commit message

---
+++ game.py
@@ -0,0 +1,4 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]
+        self.current_winner = None
---
+++ tictactoe.py
@@ -0,0 +1,12 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    
